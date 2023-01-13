e247544cff961ce4d7bd05810328a546ce0a4a9b
Your axachaSUM <markojovancevic@hotmail.rs>
2023-01-13 02:33:06 -0800

    login details changed

--- tests/MF/login.spec.js
+++ tests/MF/login.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { fillStripeInfo } from '../utils/stripe';
 
+test.setTimeout(1200000)
+
 test('Login with old user', async ({ page, context, }) => {
 
   // const tab1 = await context.newPage();
@@ -11,15 +13,16 @@ test('Login with old user', async ({ page, context, }) => {
   await page.goto('https://mfstaging.webflow.io/');
   await page.getByRole('link', { name: 'reserve FEAST' }).nth(1).click();
   await page.getByRole('link', { name: 'Reserve your feast' }).first().click();
-  await page.getByPlaceholder('Enter your email...').fill('marko.jovancevic+2812android@growthmill.com');
+  await page.getByPlaceholder('Enter your email...').fill('mjtesterovic@gmail.com');
   await page.getByRole('button', { name: 'Continue' }).click();
 
   await page.goto('https://mail.google.com/mail/u/0/#inbox');
-  await page.fill('#identifierId', 'marko.jovancevic@growthmill.com');
+  await page.fill('#identifierId', 'mjtesterovic@gmail.com');
   await page.click('#identifierNext');
   await page.waitForSelector('input[type="password"]');
-  await page.fill('input[type="password"]', 'Prastajbrt1991$');
+  await page.fill('input[type="password"]', 'Automationuser1991$');
   await page.locator('text=Next').click();
+  await page.getByRole('button', { name: 'Refresh' }).click();
   await page.waitForTimeout(5000);
   await page.getByRole('link', { name: 'Your temporary Moveable Feast login code' }).first().click();
   const pagePromise = context.waitForEvent('page');
@@ -40,7 +43,7 @@ test('Login with old user', async ({ page, context, }) => {
   await newPage.locator('button:has-text("Next")').click();
   // Click text=Select Feast size
   await newPage.locator('text=Select Feast size').click();
-  // Click #react-select-2-option-1
+  // // Click #react-select-2-option-1
   await newPage.locator('#react-select-2-option-1').click();
 
   await newPage.locator('button:has-text("Next")').click();
@@ -49,8 +52,12 @@ test('Login with old user', async ({ page, context, }) => {
 
   await newPage.locator('button:has-text("Next")').click();
 
-  await newPage.getByRole('heading', { name: 'Kuca' }).click();
+  await newPage.waitForTimeout(2000);
 
+  await newPage.locator("text=reed >> nth=0", { timeout: 10000 }).click();
+
+  // await newPage.getByRole('heading', { name: 'reed' }).click();
+  
   await newPage.locator("text=PAYMENT", { timeout: 10000 }).click();
 
   await fillStripeInfo(newPage);
@@ -58,14 +65,9 @@ test('Login with old user', async ({ page, context, }) => {
   await newPage.getByRole("button", { name: "See all orders" }).click();
 
   // await newPage.locator('button:has-text("See all orders")').click();
-  await expect(newPage).toHaveURL('https://frontend.staging.mfeast.io/member-account/my-feasts');
+  await expect(newPage).toHaveURL('https://frontend.staging.mfeast.io/member-account/account/my-feasts');
   
   await newPage.close();
 
-
-  
-
-  // click on my account button
-  // await newPage.frameLocator('#iframe-login').getByText('MY ACCOUNT').click();
 
 });
