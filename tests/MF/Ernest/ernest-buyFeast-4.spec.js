1cde947d5b18ea982cb831cbc1a3b1e089a61e82
Your axachaSUM <markojovancevic@hotmail.rs>
2023-02-05 04:28:01 -0800

    updated test

--- tests/MF/Ernest/ernest-buyFeast-4.spec.js
+++ tests/MF/Ernest/ernest-buyFeast-4.spec.js
@@ -13,11 +13,11 @@ test('Ernest buy 4 people feast e2e test', async ({ page }) => {
     //navigate
     await page.goto("https://mfstaging.webflow.io//")
 
-    await page.getByRole('link', { name: 'Learn more' }).nth(2).click();
+    await page.getByRole('link', { name: 'Learn more' }).click();
 
     await expect(page).toHaveURL('https://mfstaging.webflow.io/ernest');
 
-    await page.getByRole('link', { name: 'Reserve your feast' }).first().click();
+    await page.getByRole('link', { name: 'Reserve Now' }).first().click();
 
     await page.waitForLoadState();
 
@@ -27,10 +27,9 @@ test('Ernest buy 4 people feast e2e test', async ({ page }) => {
 
     await page.waitForLoadState();
 
-    // removed zip code valadtion i will leave it here just in case i need it again
-    // // await page.locator('[placeholder="Enter your zip..."]').fill('94114');
+    await page.getByRole('heading', { name: 'individual feast' }).click();
 
-    // await page.locator('button:has-text("Continue")').click();
+    await page.getByRole('button', { name: 'Confirm' }).click();
 
     await page.locator('[placeholder="Allergies information"]').fill('NemamAlergiju');
 
@@ -49,12 +48,6 @@ test('Ernest buy 4 people feast e2e test', async ({ page }) => {
 
     await deliveryAddress(page);
 
-    //   await page.locator('button:has-text("Submit")').click();
-    // await page.locator('button:has-text("Submit")').click();
-
-    // await page.locator('//*[@id="root"]/div[1]/div[3]/div/div/form/div/div[3]/div[2]/button').click();
-    // await page.locator('button:has-text("PAYMENT")').click();
-    //   await page.waitForLoadState();
     await page.locator('text=PAYMENT', { timeout: 10000 }).click();
 
     await page.waitForLoadState();
@@ -64,11 +57,6 @@ test('Ernest buy 4 people feast e2e test', async ({ page }) => {
     // Click button:has-text("See all orders")
     await page.locator('button:has-text("See all orders")').click();
     await expect(page).toHaveURL('https://frontend.staging.mfeast.io/member-account/account/my-feasts');
-
-    // await page.reload();
-    // await expect(page.getByText('Ordered')).toBeVisible();
-
-
 
     await page.close();
 });
