722cfd45225a30c98ececf365b200f2628986e9c
Your axachaSUM <markojovancevic@hotmail.rs>
2023-02-20 05:01:18 -0800

    update

--- tests/MF/Ernest/ernest-buyFeast-4.spec.js
+++ tests/MF/Ernest/ernest-buyFeast-4.spec.js
@@ -10,7 +10,7 @@ const randomEmail = () => {
 
 test.setTimeout(120000);
 
-// test.use({ launchOptions: { slowMo: 2000 } });
+test.use({ launchOptions: { slowMo: 2000 } });
 
 test('Ernest buy 4 people feast e2e test', async ({ page }) => {
     //navigate
